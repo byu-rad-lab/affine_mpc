d560d5f4d5ec4c17cd6277d87c6ec55afe451057
Mat Haskell <mhaskell9@gmail.com>
2026-03-28 18:09:22 -0600

    Update cnpy dependency
    - Change master branch to commit hash in case any breaking changes are
    made to master
    - Always use a static library for cnpy so that it is compiled directly
    into the affine_mpc target, no additional linking necessary

--- cmake/extern_cnpy.cmake
+++ cmake/extern_cnpy.cmake
@@ -1,24 +1,35 @@
+include(FetchContent)
+
 # cnpy requires zlib
 find_package(ZLIB REQUIRED)
 
+# Force cnpy to build statically, even if the parent project uses BUILD_SHARED_LIBS
+set(ENABLE_STATIC ON CACHE BOOL "Enable cnpy static library" FORCE)
+
 FetchContent_Declare(cnpy_extern
   GIT_REPOSITORY https://github.com/rogersce/cnpy.git
-  GIT_TAG master
+  GIT_TAG 4e8810b
 )
 
 FetchContent_MakeAvailable(cnpy_extern)
+FetchContent_GetProperties(cnpy_extern)
 
-# cnpy's CMakeLists.txt is old and doesn't set target_include_directories,
-# so we have to do it for it so the target propagates the include path.
-if(TARGET cnpy)
-  # Use FetchContent properties to get the source dir
-  FetchContent_GetProperties(cnpy_extern)
-  target_include_directories(cnpy INTERFACE ${cnpy_extern_SOURCE_DIR})
-endif()
-
-# cnpy doesn't define a namespaced target, let's create one for consistency
-if(NOT TARGET cnpy::cnpy)
-  if(TARGET cnpy)
-    add_library(cnpy::cnpy ALIAS cnpy)
+# Verify the static target was created
+if(TARGET cnpy-static)
+  # CRITICAL: Must be compiled with -fPIC to be linked into our shared library
+  set_target_properties(cnpy-static PROPERTIES POSITION_INDEPENDENT_CODE ON)
+  
+  # Ensure the target propagates the include path
+  target_include_directories(cnpy-static INTERFACE ${cnpy_extern_SOURCE_DIR})
+  
+  # Link ZLIB. We use PUBLIC so affine_mpc transitively gets the ZLIB link 
+  # needed to resolve the static symbols inside cnpy-static.
+  target_link_libraries(cnpy-static PUBLIC ZLIB::ZLIB)
+  
+  # Create our standard namespaced alias
+  if(NOT TARGET cnpy::cnpy)
+    add_library(cnpy::cnpy ALIAS cnpy-static)
   endif()
+else()
+  message(FATAL_ERROR "cnpy-static target not found!")
 endif()
